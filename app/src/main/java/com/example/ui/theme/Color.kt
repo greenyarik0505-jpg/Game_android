4ed3eab533ab9dcae870467b20790938e636cfc6
greenyarik0505-jpg <greenyarik0505@gmail.com>
2026-08-03 08:49:22 +0300

    Upload app/src/main/java/com/example/ui/theme/Color.kt

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NeonCyan = Color(0xFF00E5FF)
+val NeonPink = Color(0xFFFF007A)
+val NeonAmber = Color(0xFFFFB300)
+val NeonEmerald = Color(0xFF00E676)
+val OverheatCrimson = Color(0xFFFF3D00)
+val DeepSpaceBackground = Color(0xFF090A1A)
+val DarkSurfaceCard = Color(0xFF131738)
+val DarkSurfaceCardBorder = Color(0xFF2B336B)
+val TextPrimary = Color(0xFFEEF2FF)
+val TextSecondary = Color(0xFF9EAAD1)
+val GravRingViolet = Color(0xFFB388FF)
